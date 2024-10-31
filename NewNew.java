1f6d3341c3e53727efa57b3634430cb78275b65f
AndresFiRu <andres.fierroruiz@edu.fh-joanneum.at>
2024-10-31 14:32:49 +0100

    There is a new class: Hello Andrey

--- NewNew.java
+++
@@ -1,4 +0,0 @@
-public class NewNew {
-
-    System.out.println
-}
---
+++ src/HalloDaniela.java
@@ -0,0 +1,6 @@
+public class HalloDaniela {
+    public static void main(String[] args) {
+        System.out.println("Daniela das ist unseren neen Class");
+    }
+}
+
