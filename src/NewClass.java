1e911f9cdefe59699897b6456af2c285af9c41f6
AndresFiRu <andres.fierroruiz@edu.fh-joanneum.at>
2024-10-31 14:23:38 +0100

    NewClass is now a real class

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+public class NewClass {
+
+  public static void main(String[] args) {
+
+    System.out.println("wir sind die besten");
+
+    // Berechne die Summe von zwei Zahlen und gebe das Ergebnis aus
+    int zahl1 = 5;
+    int zahl2 = 10;
+    int summe = zahl1 + zahl2;
+    System.out.println("Die Summe ist: " + summe);
+
+  }
+}
+
