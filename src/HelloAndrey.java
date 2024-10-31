e24252972a3e0d143017d64d50f3146920e9e5c5
AndresFiRu <andres.fierroruiz@edu.fh-joanneum.at>
2024-10-31 14:32:49 +0100

    There is a new class: Hello Andrey

---
+++ src/HelloAndrey.java
@@ -0,0 +1,12 @@
+public class HelloAndrey {
+    public static void main(String[] args) {
+        // Prüfe, ob eine Zahl gerade ist und gebe das Ergebnis aus
+        int zahl = 8;
+        if (zahl % 2 == 0) {
+            System.out.println("Die Zahl " + zahl + " ist gerade.");
+        } else {
+            System.out.println("Die Zahl " + zahl + " ist ungerade.");
+        }
+    }
+
+}
